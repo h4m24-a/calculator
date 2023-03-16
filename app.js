c1c76f936bdea18d331f2bfdd30180b7defedaa3
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-16 21:40:21 +0000

    added global variables and selected buttons for DOM

---
+++ app.js
@@ -0,0 +1,14 @@
+// Global Variables
+let previousValue = '';
+let currentValue = '';
+let operator = '';
+
+// Selecting buttons for DOM
+let numbers = document.querySelectorAll('.number');
+let equal = document.querySelectorAll('.equal');
+let operators = document.querySelectorAll('.operator');
+let decimal = document.querySelectorAll('.decimal');
+let clear = document.querySelector('clear-btn');
+
+let previousScreen = document.querySelector('previous');
+let currentScreen = document.querySelector('current')
