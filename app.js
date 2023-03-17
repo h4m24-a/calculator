fb03b073d8b3dad1c27b2d6fdda8ac5972ba9211
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-03-17 22:11:58 +0000

    added function for rounding numbers down and for numbers and operators to work with the display screen

--- app.js
+++ app.js
@@ -12,18 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentDisplay = document.querySelector(".current");
   let previousDisplay = document.querySelector(".previous");
 
+
   // Looping through numbers
   numbers.forEach((number) => number.addEventListener("click", function (e) {
       handleNumber(e.target.textContent);
       currentDisplay.textContent = currentValue;
     }));
 
+
   // Looping through operators
   operators.forEach((op) => op.addEventListener("click", function(e){
     handleOperator(e.target.textContent)
     previousDisplay.textContent = previousValue + " " + operator;
     currentDisplay.textContent = currentValue;
-}));
+  }));
 
    // Clear Button
    clear.addEventListener("click", function () {
@@ -34,21 +36,56 @@ document.addEventListener("DOMContentLoaded", function () {
     currentDisplay.textContent = currentValue;
   });
 
+  
+  // Decimal Button
+
+
+  // Equals Button
+    equal.addEventListener('click', function(){
+      operate();
+      previousDisplay.textContent = '';
+      currentDisplay.textContent = previousValue;
+    })
+
 });
 
-
-
-
   
-  function handleNumber(num) {
-    if (currentValue.length <= 5) {
+function handleNumber(num) {
+  if(currentValue.length <= 5) {
       currentValue += num;
     }
-  }
+}
 
 
-  function handleOperator(op) {
-    operator = op;
-    previousValue = currentValue;
-    currentValue = '';
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = '';
+}
+
+
+function operate(){
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+
+  if(operator === "+"){
+      previousValue += currentValue;
+  } else if(operator === "-"){
+      previousValue -= currentValue;
+  } else if(operator === "*"){
+      previousValue *= currentValue;
+  } else{
+      previousValue /= currentValue;
   }
+
+  previousValue = roundNum(previousValue);
+  previousValue = previousValue.toString();
+  currentValue = previousValue.toString();
+}
+
+
+
+  // Rounding Function
+function roundNum(num){
+  return Math.round(num * 1000) / 1000;
+}
